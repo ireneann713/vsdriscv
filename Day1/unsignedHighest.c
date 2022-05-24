239aff313dacd047ed7683d4db8e903f27a2dd81
ireneann713 <55539862+ireneann713@users.noreply.github.com>
2022-05-24 20:44:09 +0530

    Update unsignedHighest.c

--- Day1/unsignedHighest.c
+++ Day1/unsignedHighest.c
@@ -1,1 +1,9 @@
+#include<stdio.h>
+  #include<math.h>
+
+  int main(){
+          unsigned long long int max =(unsigned long long int)(pow(2,64) -1);
+          printf("Highest number represented by unsigned long long int %llu\n",max);
+         return 0;
+  }
 
